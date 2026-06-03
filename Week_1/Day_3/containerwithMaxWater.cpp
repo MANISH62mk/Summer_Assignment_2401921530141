5769355ef4c8871a7637f5bc903258917039f59f
Manish kumar <manish62017mk@gmail.com>
2026-06-03 12:27:12 +0530

    week1/Day3

---
+++ Week_1/Day_3/containerwithMaxWater.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int water = 1;
+        int maxWater = INT_MIN;
+
+        int j=height.size()-1;
+
+        for(int i=0;i<height.size();){
+            
+            if(height[i]<=height[j]){
+                water= height[i]*(j-i);
+                i++;
+                maxWater = max(maxWater,water);
+
+            }
+            else{
+                water= height[j]*(j-i);
+                j--;
+                maxWater = max(maxWater,water);
+
+            }
+        }
+        return maxWater;
+        
+    }
+};
---
+++ Week_1/Day_3/squareofsortedarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> arr;
+        for(int i=0;i<nums.size();i++){
+            arr.push_back(nums[i]*nums[i]);
+        }
+        sort(arr.begin(),arr.end());
+
+        // for(int i=0;i<nums.size();i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // sort(nums.begin(),nums.end());
+        
+        return arr;
+    }
+};
