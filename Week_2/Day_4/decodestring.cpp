681236a03107c4ef277b176286bae1d778ce7da2
Manish kumar <manish62017mk@gmail.com>
2026-06-12 23:22:08 +0530

    week2/day4

---
+++ Week_2/Day_4/decodestring.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> numStack;
+        stack<string> strStack;
+
+        string curr = "";
+        int num = 0;
+
+        for (char ch : s) {
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            else if (ch == '[') {
+                numStack.push(num);
+                strStack.push(curr);
+                num = 0;
+                curr = "";
+            }
+            else if (ch == ']') {
+                int k = numStack.top();
+                numStack.pop();
+
+                string temp = curr;
+
+                for (int i = 1; i < k; i++) {
+                    curr += temp;
+                }
+
+                curr = strStack.top() + curr;
+                strStack.pop();
+            }
+            else {
+                curr += ch;
+            }
+        }
+
+        return curr;
+    }
+};
---
+++ Week_2/Day_4/reversewords.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string word;
+        vector<string> words;
+
+        for (char ch : s) {
+            if (ch != ' ') {
+                word += ch;
+            } else if (!word.empty()) {
+                words.push_back(word);
+                word = "";
+            }
+        }
+
+        if (!word.empty()) {
+            words.push_back(word);
+        }
+
+        string ans = "";
+
+        for (int i = words.size() - 1; i >= 0; i--) {
+            ans += words[i];
+            if (i > 0) {
+                ans += " ";
+            }
+        }
+
+        return ans;
+    }
+};
